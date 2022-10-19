531d8e754c2d9ec9e112a391ce0ed8fb24f00a9b
Lomner-first1 <klukva06@gmail.com>
2022-10-19 20:47:57 +0300

    blabla

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./nav.module.css";
 
-export default function nav() {
+export default function Nav() {
   return (
 
     <div className = {styles.nav}>
