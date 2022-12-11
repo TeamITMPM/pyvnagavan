aa8d8cd338da6a27b56e0b4de6cac20f81f3c253
makkob <maximkabig945@gmail.com>
2022-12-12 01:19:10 +0200

    store

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ export default function App() {
       <Header />
 
       <Route path="/" component={Shop} exact />
+      {/* <Route path="/" component={Shop} exact /> */}
 
       <Footer />
     </Router>
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-// import { useDispatch } from "react-redux/es/exports";
+import React, { useEffect } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
 
 import { connect } from "react-redux";
@@ -10,48 +10,48 @@ import styles from "./items.module.css";
 const Items = () => {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.itemState);
-  console.log(productList);
+  const { products, loading } = productList;
+
   useEffect(() => {
     dispatch(listItems());
-    // listItems();
   }, []);
 
-  // console.log(items);
   return (
-    // <div className={styles.products}>
-    //   <div className={styles.items}>
-    //     {items.map(({ name, id, price , oldPrice , rating, img}) => {
-    //       return (
-    //         <div className={styles.item} key={id}>
-    //           <h2>{name}</h2>
-    //           <img
-    //             className={styles.itemsIMG}
-    //             // src={require("../../db/img/7cfa27f3-0a3d-469a-b9a5-5451534bccc8.png")}
-    //             src={
-    //               img
-    //                 ? require(`../../../../back_pyvnagavan/static/${img}`)
-    //                 : "https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg"
-    //             }
-    //             alt={name}
-    //           />
-    //           <p>Стара ціна за літр {oldPrice}</p>
-    //           <p>Ціна за 1 літр {price} грн</p>
-    //           <p>Рейтинг {rating}</p>
-    //           <button type="button" className={styles.button}>
-    //             Заказать{" "}
-    //           </button>
-    //         </div>
-    //       );
-    //     })}
-    //   </div>
-    // </div>
+    <div className={styles.products}>
+      <div className={styles.items}>
+        {products &&
+          products.map(({ name, id, price, oldPrice, rating, img }) => {
+            return (
+              <div className={styles.item} key={id}>
+                <h2>{name}</h2>
+                <img
+                  className={styles.itemsIMG}
+                  // src={require("../../db/img/7cfa27f3-0a3d-469a-b9a5-5451534bccc8.png")}
+                  src={
+                    img
+                      ? require(`../../../../back_pyvnagavan/static/${img}`)
+                      : "https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg"
+                  }
+                  alt={name}
+                />
+                <p>Стара ціна за літр {oldPrice}</p>
+                <p>Ціна за 1 літр {price} грн</p>
+                <p>Рейтинг {rating}</p>
+                <button type="button" className={styles.button}>
+                  Заказать{" "}
+                </button>
+              </div>
+            );
+          })}
+        {loading && <h1> Завантажується пивко ... </h1>}
+      </div>
+    </div>
 
-    <h1>Items</h1>
+    // <h1>Items</h1>
   );
 };
 
 let mapStateToProps = (state) => {
-  // console.log("SSS", state);
   return {
     items: state.itemState.items,
   };
