0373bb92ff316e31e90c89c52e8009e1aa796264
makkob <maximkabig945@gmail.com>
2022-12-12 14:42:59 +0200

    login

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
 import Header from "./Header";
 import Shop from "../pages/Shop";
-import Nav from "./Nav";
-import News from "./News";
-import Items from "./Items";
+import Auth from "../pages/Auth";
+// import Nav from "./Nav";
+// import News from "./News";
+// import Items from "./Items";
 import Footer from "./Footer";
 // import itemsDB from "../db/item/item.json";
 import fetchItemsFromDB from "../services/services";
 
 export default function App() {
+  const isAuth = true;
   return (
     <Router>
-      <Header />
-
-      <Route path="/" component={Shop} exact />
-      {/* <Route path="/" component={Shop} exact /> */}
-
-      <Footer />
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => {
+          <Route key={path} path={path} component={Component} exact />;
+        })}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+      {/* <Redirect to={SHOP_ROUTE} /> */}
     </Router>
   );
 }
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Auth = () => {
-  return <div>LOGIN!!!</div>;
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="email" name="email" />
+        <br />
+
+        <input type="password" placeholder="password" name="password" />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
