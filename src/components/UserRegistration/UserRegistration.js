0dba1e0cc76ce77e6523dfe05c805ded980ef044
michaelkobetss <michael.kobetss@gmail.com>
2022-12-14 19:24:11 +0200

    signup

--- src/components/UserRegistration/UserRegistration.js
+++ src/components/UserRegistration/UserRegistration.js
@@ -11,26 +11,16 @@ export default function UserRegistration() {
   return (
     <div className={styles.modal}>
       <button className={styles.button1}>
-        <a href="">
-          <h1 className={styles.h1}>
-            Увійти
-            </h1>
-            </a>
+        <h1 className={styles.h1}>Увійти</h1>
       </button>
-      <button className={styles.button2}>
-        <a href="">
-          <h1 className={styles.h1}>
-            Зареєструватись
-            </h1>
-        </a>
+      <button className={styles.button2} autoFocus>
+        <h1 className={styles.h1}>Зареєструватись</h1>
       </button>
       <form className={styles.form} action="">
         <div className={styles.FormRegistration}>
           <div className={styles.labelTable}>
             <label htmlFor="textSurname">
-              <h2 className={styles.h2}>
-                Surname
-              </h2>
+              <h2 className={styles.h2}>Фамілія</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -38,9 +28,7 @@ export default function UserRegistration() {
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="textName">
-              <h2 className={styles.h2}>
-                TextName
-              </h2>
+              <h2 className={styles.h2}>Ім'я</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -48,20 +36,20 @@ export default function UserRegistration() {
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="numTel">
-              <h2 className={styles.h2}>
-                tel
-              </h2>
+              <h2 className={styles.h2}>Телефон</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
-            <input className={styles.inputText}
-              id="3" type="tel" pattern="[+]{1}-[3]{1}-[8]{1}-[(]{1}-[0-9]{3}-[)]{1}-[0-9]{3}-[0-9]{2}-[0-9]{2}"/>
+            <input
+              className={styles.inputText}
+              id="3"
+              type="tel"
+              pattern="[+]{1}\[3]{1}\[8]{1}\[(]{1}\[0-9]{3}\[)]{1}\[0-9]{3}-[0-9]{2}-[0-9]{2}"
+            />
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="mail">
-              <h2 className={styles.h2}>
-                mail
-                </h2>
+              <h2 className={styles.h2}>Email</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -69,21 +57,23 @@ export default function UserRegistration() {
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="date">
-              <h2 className={styles.h2}>
-                date
-                </h2>
+              <h2 className={styles.h2}>Дата народження</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
-            <input className={styles.inputText} id="5" type="date" value="2004-09-1" max={date18}/>
-              {/* //дату на 18 лет до текущей
+            <input
+              className={styles.inputText}
+              id="5"
+              type="date"
+              value="2004-09-1"
+              max={date18}
+            />
+            {/* //дату на 18 лет до текущей
             // max="2004-02-01" */}
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="favoritebeer">
-              <h2 className={styles.h2}>
-                beer
-                </h2>
+              <h2 className={styles.h2}>Улюблене пиво</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -91,9 +81,7 @@ export default function UserRegistration() {
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="password">
-              <h2 className={styles.h2}>
-                password
-                </h2>
+              <h2 className={styles.h2}>Пароль</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -101,21 +89,19 @@ export default function UserRegistration() {
           </div>
           <div className={styles.labelTable}>
             <label htmlFor="confirmpassword">
-              <h2 className={styles.h2}>
-                confirmpassword
-                </h2>
+              <h2 className={styles.h2}>Повторіть пароль</h2>
             </label>
           </div>
           <div className={styles.inputTable}>
-            <input className={styles.inputConfirmPassword} type="password"id="7"/>
+            <input
+              className={styles.inputConfirmPassword}
+              type="password"
+              id="7"
+            />
           </div>
         </div>
-        <div >
-          <button className={styles.buttonFinish}>
-            <h1>
-              END
-              </h1>
-              </button>
+        <div>
+          <input className={styles.buttonFinish} type="submit" />
         </div>
       </form>
     </div>
