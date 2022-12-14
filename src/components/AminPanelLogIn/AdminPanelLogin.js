7660cf3769a766c5433ed3947cabb75dc6d0c5f5
makkob <maximkabig945@gmail.com>
2022-12-14 18:50:10 +0200

    test

--- src/components/AminPanelLogIn/AdminPanelLogin.js
+++ src/components/AminPanelLogIn/AdminPanelLogin.js
@@ -1,45 +1,31 @@
 // import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
-import styles from "./AdminPanelLogin.module.css"
+import styles from "./AdminPanelLogin.module.css";
 
 console.log(styles);
 export default function AdminPanelLogIn() {
   return (
     <div className={styles.modal}>
       <button className={styles.button1}>
-          <p className={styles.p1}>Увійти</p>
+        <p className={styles.p1}>Увійти</p>
       </button>
       <button className={styles.button2}>
-          <p className={styles.p1}>Зареєструватись</p>
+        <p className={styles.p1}>Зареєструватись</p>
       </button>
       <form className={styles.form} action="">
         <div className={styles.FormRegistration}>
-        <div className={styles.labelTable}>
+          <div className={styles.labelTable}>
             <label htmlFor="mail">
-              <p className={styles.p2}>
-                Email
-                </p>
+              <p className={styles.p2}>Email</p>
             </label>
           </div>
           <div className={styles.inputTable}>
             <input className={styles.inputText} id="4" type="email" />
           </div>
-          <div className={styles.labelTable}>
-            <label htmlFor="numTel">
-              <p className={styles.p2}>
-                Телефон
-              </p>
-            </label>
-          </div>
-          <div className={styles.inputTable}>
-            <input className={styles.inputText}
-              id="3" type="tel" pattern="[+]{1}\[3]{1}\[8]{1}\[(]{1}\[0-9]{3}\[)]{1}\[0-9]{3}-[0-9]{2}-[0-9]{2}"/>
-          </div>
+
           <div className={styles.labelTable}>
             <label htmlFor="password">
-              <p className={styles.p2}>
-                Пароль
-                </p>
+              <p className={styles.p2}>Пароль</p>
             </label>
           </div>
           <div className={styles.inputTable}>
@@ -47,7 +33,7 @@ export default function AdminPanelLogIn() {
           </div>
         </div>
         <div>
-          <input className={styles.buttonFinish} type = "submit" />
+          <input className={styles.buttonFinish} type="submit" />
         </div>
       </form>
     </div>
