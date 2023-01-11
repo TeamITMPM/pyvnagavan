3cf5523aa916e48cbe913720cfc42775ace68840
makkob <maximkabig945@gmail.com>
2023-01-11 03:20:32 +0200

    basket fix

--- src/components/BasketComponent/BeerInBasket/BeerInBasket.js
+++ src/components/BasketComponent/BeerInBasket/BeerInBasket.js
@@ -5,65 +5,73 @@ import styles from "./BeerInBasket.module.css";
 
 export default function BeerInBasket() {
   const dispatch = useDispatch();
-  const productListInBasket = useSelector((state) => state.basketState);
+  const { basketState } = useSelector((state) => state);
+  console.log(basketState);
+  const items = basketState.itemInBasket;
+  // const totalPrice = useSelector((state) => state.basketState.items[1]);
   const productList = useSelector((state) => state.itemState);
   const { products, loading } = productList;
-  const { itemInBasket } = productListInBasket;
-  const [finalBasketItem, setFinalBasketItem] = useState([]);
 
+  // const [finalBasketItem, setFinalBasketItem] = useState([]);
   useEffect(() => {
     dispatch(listMyBasket());
   }, []);
 
-  useEffect(() => {
-    if (itemInBasket && products) {
-      const final = itemInBasket.map((cartItem) => {
-        const product = products.find(({ id }) => {
-          return id === cartItem.itemId;
-        });
-        if (product) {
-          return {
-            ...cartItem,
-            metaData: product,
-          };
-        }
-        return cartItem;
-      });
-      setFinalBasketItem(final);
-    }
-  }, [itemInBasket, products]);
+  // useEffect(() => {}, [items]);
 
-  console.log("finalBasketItem>>>>>", finalBasketItem);
-  return (
-    <div className={styles.items}>
-      {finalBasketItem &&
-        finalBasketItem.map(({ metaData, quantity }) => {
-          const { id, img, name, price } = metaData;
-          
-          console.log("metadata >>>>>", metaData);
-          return (
-            <div key={id} className={styles.item}>
-              <img
-                src={
-                  img
-                    ? require(`../../../../../back_pyvnagavan/static/${img}`)
-                    : "https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg"
-                }
-                className={styles.img}
-              />
-              <h2 className={styles.header}>{name}</h2>
+  // useEffect(() => {
+  //   if (items && products) {
+  //     const final = items.map((cartItem) => {
+  //       const product = products.find(({ id }) => {
+  //         return id === cartItem.itemId;
+  //       });
+  //       if (product) {
+  //         return {
+  //           ...cartItem,
+  //           metaData: product,
+  //         };
+  //       }
+  //       return cartItem;
+  //     });
+  //     setFinalBasketItem(final);
+  //   }
+  // }, [items, products]);
 
-              <p className={styles.text}>
-                <b>Ціна за 1 літр:</b> {price} грн,
-              </p>
-              <p>
-                <b>Кількість:</b> {quantity} літрів
-              </p>
-              <p className={styles.text}> </p>
-            </div>
-          );
-        })}
-      {loading && <h1> Завантажується пивко ... </h1>}
-    </div>
-  );
+  if (!basketState.loading && basketState.itemInBasket) {
+    return (
+      <div className={styles.items}>
+        {items[0] &&
+          items[0].map(({ product, dataValues }) => {
+            const { id, img, name, price } = product;
+            const { quantity } = dataValues;
+
+            return (
+              <div key={id} className={styles.item}>
+                <img
+                  src={
+                    img
+                      ? require(`../../../../../back_pyvnagavan/static/${img}`)
+                      : "https://img.freepik.com/free-vector/glitch-error-404-page_23-2148105404.jpg"
+                  }
+                  className={styles.img}
+                />
+                <h2 className={styles.header}>{name}</h2>
+
+                <p className={styles.text}>
+                  <b>Ціна за 1 літр:</b> {price} грн,
+                </p>
+                <p>
+                  <b>Кількість:</b> {quantity} літрів
+                </p>
+                <p className={styles.text}> </p>
+              </div>
+            );
+          })}
+        {loading && <h1> Завантажується пивко ... </h1>}
+      </div>
+    );
+  }
+  if (basketState.loading || !basketState.itemInBasket) {
+    return <div> Завантажується корзина</div>;
+  }
 }
