7e3fa44155dd8fb9179e8626aa85102ac4a35666
michaelkobetss <michael.kobetss@gmail.com>
2023-02-13 18:56:43 +0200

    TEAMMPM-40 <Вот работаю>

---
+++ src/components/OrderHistory/OrderHistory.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Table } from "react-bootstrap";
+
+import styles from "./OrderHistory.module.css"
+
+
+export default function OrderHistory() {
+  return <div>
+    Список замовлень
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Унікальний номер замовлення</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  </div>;
+}
--- src/pages/User.js
+++ src/pages/User.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import UserInfo from "../components/UserInfo";
+import OrderHistory from "../components/OrderHistory";
 // import { connect } from "react-redux";
 
 export default function User() {
@@ -12,6 +13,8 @@ export default function User() {
       <Header />
 
       <UserInfo />
+      <OrderHistory />
+
       <div style={{bottom: "0", width: "100%", height:"35vh"}} />
 
       <Footer />
