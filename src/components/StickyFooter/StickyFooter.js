a54354e16818b63d0e530f8ffd00c6edc85d1aaa
michaelkobetss <michael.kobetss@gmail.com>
2023-02-14 16:11:10 +0200

    TEAMMPM-56 <sticky-footer-done>

--- src/components/StickyFooter/StickyFooter.js
+++ src/components/StickyFooter/StickyFooter.js
@@ -1,20 +1,28 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhone,
+  faArrowUp,
+  faArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./StickyFooter.module.css";
 
 export default function StickyFooter() {
-  const phoneIcon = (
-    <FontAwesomeIcon
-      icon={faPhone}
-      color="black"
-      className={styles.phoneIcon}
-    />
+  // const phoneIcon = (
+  //   <FontAwesomeIcon
+  //     icon={faPhone}
+  //     color="black"
+  //     className={styles.phoneIcon}
+  //   />
+  // );
+  const arrowUpIcon = (
+    <FontAwesomeIcon icon={faArrowUp} color="#efeee9" size="xl"  className={styles.arrow} />
   );
-  const arrowUpIcon = <FontAwesomeIcon icon={faArrowUp} color="#efeee9" size="xl" />;
-  const arrowDownIcon = <FontAwesomeIcon icon={faArrowDown} color="#efeee9" size="xl" />;
+  const arrowDownIcon = (
+    <FontAwesomeIcon icon={faArrowDown} color="#efeee9" size="xl" className={styles.arrow}/>
+  );
   const scrollToFooter = () => {
     const footer = document.getElementById("footer");
     footer.scrollIntoView({
@@ -33,13 +41,27 @@ export default function StickyFooter() {
   };
   return (
     <footer className={styles.topfooter}>
-    <button className={styles.anchor} onClick = {()=>{scrollToFooter()}}>{arrowDownIcon}</button>
+      <button
+        className={styles.buttonLeft}
+        onClick={() => {
+          scrollToFooter();
+        }}
+      >
+        {arrowDownIcon}
+      </button>
       <div>
         <a href="" className={styles.number}>
-       +380639356317
+          +380639356317
         </a>
       </div>
-      <button className={styles.anchor} onClick = {()=>{scrollToHeader()}}>{arrowUpIcon}</button>
+      <button
+        className={styles.buttonRight}
+        onClick={() => {
+          scrollToHeader();
+        }}
+      >
+        {arrowUpIcon}
+      </button>
     </footer>
   );
 }
