49503ed37405f88ca8ff5d0f4bf1ecb4c3d65c89
makkob <maximkabig945@gmail.com>
2023-04-12 18:05:11 +0300

    adress

--- src/actions/adressAction.js
+++ src/actions/adressAction.js
@@ -1,26 +1,24 @@
-// import axios from "axios";
-// import {
-//   ADRESS_LIST_FAIL,
-//   ADRESS_LIST_REQUEST,
-//   ADRESS_LIST_SUCCESS,
-// } from "../constants/adressConstants";
+import axios from "axios";
+import {
+  ADRESS_LIST_FAIL,
+  ADRESS_LIST_REQUEST,
+  ADRESS_LIST_SUCCESS,
+} from "../constants/adressConstants";
 
 export const getAdress = () => async (dispatch) => {
-  console.log("WWWWWWWWW");
+  const URL = process.env.REACT_APP_API_URL + `api/shop`;
+  try {
+    dispatch({ type: ADRESS_LIST_REQUEST });
+    const { data } = await axios.get(URL);
 
-  //   const URL = process.env.REACT_APP_API_URL + `api/shop`;
-  //   try {
-  //     dispatch({ type: ADRESS_LIST_REQUEST });
-  //     const { data } = await axios.get(URL);
-  //     console.log("data", data);
-  //     dispatch({
-  //       type: ADRESS_LIST_SUCCESS,
-  //       payload: data,
-  //     });
-  //   } catch (error) {
-  //     dispatch({
-  //       type: ADRESS_LIST_FAIL,
-  //       payload: error,
-  //     });
-  //   }
+    dispatch({
+      type: ADRESS_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ADRESS_LIST_FAIL,
+      payload: error,
+    });
+  }
 };
--- src/reducers/adressReducers.js
+++ src/reducers/adressReducers.js
@@ -4,14 +4,14 @@ import {
   ADRESS_LIST_SUCCESS,
 } from "../constants/adressConstants";
 
-export const adressReducer = (state = { types: [], category: {} }, action) => {
+export const adressReducer = (state = {}, action) => {
   switch (action.type) {
     case ADRESS_LIST_REQUEST:
       return { loading: true, types: [] };
     case ADRESS_LIST_SUCCESS:
       return {
         loading: false,
-        types: action.payload,
+        adress: action.payload,
       };
 
     case ADRESS_LIST_FAIL:
